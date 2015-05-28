391b40cfe60f1a1cd690441d2fcef851b3a797dc
Sabina Shrestha <sabina.pari@yahoo.com>
2015-05-28 12:37:54 -0600

    add

--- magic_eight.rb
+++ magic_eight.rb
@@ -1,14 +1,20 @@
 class MagicEight
 	def get_question
+    while true
 		puts "Ask me a question."
 		user_input = gets.chomp
 		answer
+    puts "wanna ask again?? yes / no"
+    again = gets.chomp.upcase
+    get_question if again == "YES"
+    puts "See you again."; break if again == "NO"
+    end
   end
 
 	def answer
 		computer_answer = ["yes", "no", "outlook is good", "don't count on it", "not in a million years", "it is certain"].sample
 		puts "The answer is #{computer_answer}."	
-end
+  end
 end
 
 me = MagicEight.new
