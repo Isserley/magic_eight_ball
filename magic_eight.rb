7f41f5c8633d925fb87ecc6d1f5064aa9c33dce8
Tiffany Forsyth <tiffanyforsyth1@gmail.com>
2015-05-28 12:16:54 -0600

    added computer answer method

--- magic_eight.rb
+++ magic_eight.rb
@@ -2,5 +2,14 @@ class MagicEight
 	def get_question
 		puts "Ask me a question."
 		user_input = gets.chomp
+		answer
   end
+
+	def answer
+		computer_answer = ["yes", "no", "outlook is good", "don't count on it", "not in a million years", "it is certain"].sample
+		puts "The answer is #{computer_answer}."	
 end
+end
+
+me = MagicEight.new
+me.get_question
